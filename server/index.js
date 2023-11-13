4cbade8f9ac8dc740d8f794a4a1c3257502b6845
WintrCat <89023414+WintrCat@users.noreply.github.com>
2023-11-13 15:12:39 +0000

    Added file uploads sort of

--- server/index.js
+++ server/index.js
@@ -1,16 +1,28 @@
 const express = require("express");
-const fileUpload = require("express-fileupload");
+const multer = require("multer");
 const path = require("path");
 
 const config = require("./config.json");
 
 const app = express();
-
-app.use(fileUpload({
-    "limits": {"fileSize": 64 * 1024 * 1024}
-}));
+const fileParser = multer({
+    storage: multer.diskStorage({
+        filename: (req, file, save) => {
+            save(null, file.originalname);
+        },
+        destination: (req, file, save) => {
+            save(null, path.resolve("media"));
+        }
+    })
+});
 
 app.use("/", express.static("client"));
+
+app.get("/", async (req, res) => {
+
+    res.sendFile(path.resolve("client/index.html"));
+
+});
 
 app.get("/upload", async (req, res) => {
 
@@ -18,11 +30,15 @@ app.get("/upload", async (req, res) => {
 
 });
 
-app.post("/upload", async (req, res) => {
+app.post("/upload", fileParser.single("UploadedVideo"), async (req, res) => {
 
-    let video = req.files["UploadedVideo"];
+    let video = req.file;
 
-    console.log(`file received! ${video.name} with size ${video.size} bytes`);
+    console.log(video);
+
+    console.log(`file received! ${video.originalname} with size ${video.size} bytes`);
+
+    res.redirect("/");
 
 });
 
