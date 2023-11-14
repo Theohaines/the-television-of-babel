70febfbece0f4f0171472363a8e97b9bce186e9a
Theohaines <91698052+Theohaines@users.noreply.github.com>
2023-11-14 22:50:25 +0000

    Committing

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,7 +1,8 @@
 const VideoSrc = document.getElementById("VideoSrc");
 const ImageSrc = document.getElementById("ImageSrc");
+let darkModeEnabled = false;
 
-function getVideo(){
+function getVideo(){ //Shit I don't understand (If it breaks I will ask Wilson about it lol)
     fetch("/getVideo", {
         "method": "GET"
     }).then(async res => {
@@ -10,11 +11,33 @@ function getVideo(){
     });
 }
 
-function ToggleVideo(){
+function toggleVideo(){
     setTimeout(() => {
         ImageSrc.style.display = "none";
     }, 2000);
 }
 
+// ***** SITE ALWAYS IN DARK MODE NOW ***** 
+
+/* function toggleDarkMode(){ // I HATE GOOD INTERFACE DESIGN 
+    if (darkModeEnabled == false){
+        // Change these elements to dark colours
+        darkModeEnabled = true;
+    } else {
+        // Change these elements to light colours 
+        darkModeEnabled = false;
+    }
+} */
+
+function toggleElement(ID){ // Reusable way to toogle element visibility via ID tag (only works on block elements)
+    var element = document.getElementById(ID);
+
+    if (element.style.display == "none"){
+        element.style.display == "block";
+    } else {
+        element.style.display == "none";
+    }
+}
+
 getVideo()
-ToggleVideo();
+toggleVideo();
