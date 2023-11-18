d87b2531a2255c414995c1c44d5966fd149d316d
Theohaines <91698052+Theohaines@users.noreply.github.com>
2023-11-18 21:36:37 +0000

    Mobile stuff

--- client/scripts/reporting.js
+++ client/scripts/reporting.js
@@ -1,11 +1,14 @@
 //REPORTING SHITE
 const reportMenu = document.getElementById("ReportMenu");
+const videoContainer = document.getElementById("VideoContainer");
 
 function toggleReportMenu(){
     if (reportMenu.style.display == "block"){
         reportMenu.style.display = "none";
+        videoContainer.style.display = "block";
     } else {
         reportMenu.style.display = "block";
+        videoContainer.style.display = "none";
     }
 }
 
