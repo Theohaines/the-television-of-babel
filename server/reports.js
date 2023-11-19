d506c6008a5e9e1ec0be734710ae919cdc6d1bbe
WintrCat <wintrcat.wilson@gmail.com>
2023-11-19 00:19:30 +0000

    Added report acceptance debugging

--- server/reports.js
+++ server/reports.js
@@ -40,18 +40,24 @@ router.get("/get", async (req, res) => {
 
 router.post("/accept", async (req, res) => {
 
+    console.log("accept request received");
+
     try {
         var videoId = req.body.uuid;
         var timestamp = req.body.timestamp;
+        console.log("videoId and timestamp in accept request calculated")
     } catch (err) {
+        console.log(err.toString());
         return res.sendStatus(400);
     }
 
     if (req.auth) {
         fs.rmSync("media/" + videoId);
+        console.log("removed from media successfully")
         clearReport(videoId);
         res.sendStatus(200);
     } else {
+        console.log("no req.auth so yeah")
         res.sendStatus(400);
     }
 
