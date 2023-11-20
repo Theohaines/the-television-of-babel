ee7caeab2853cae8eb3d623f4105738b7eab40bc
Theohaines <91698052+Theohaines@users.noreply.github.com>
2023-11-20 14:58:45 +0000

    Update reports.js

--- server/reports.js
+++ server/reports.js
@@ -24,8 +24,8 @@ router.post("/create", async (req, res) => {
 
     database.prepare("INSERT INTO reports (uuid, reason, timestamp) VALUES (?, ?, ?)").run(videoId, reportReason, Date.now());
 
-    res.send("Thank you for your report. It will be reviewed soon.");
-
+    window.href = "https://theohaines.xyz";
+    alert("Thankyou for your report, it will be reviewed soon.");
 });
 
 router.get("/get", async (req, res) => {
